c736099a0847bb62aa0b3d86f00c8c412e76b3a9
Swagat-Kumar <swagat.kumar.code@gmail.com>
2020-10-04 14:48:52 +0530

    Ugly Number GFG

---
+++ DynamicProgramming/uglyNumber.java
@@ -0,0 +1,35 @@
+package DynamicProgramming;
+
+import java.util.Scanner;
+
+class Solution {
+    long getNthUglyNo(int n) {
+        // code here
+        long[] listA = new long[10001];
+        int c = 0;
+        listA[c] = 1;
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while (c < n) {
+            long nextU = Math.min(listA[i] * 2, listA[j] * 3);
+            nextU = Math.min(nextU, listA[k] * 5);
+            if (nextU == listA[i] * 2)
+                i++;
+            if (nextU == listA[j] * 3)
+                j++;
+            if (nextU == listA[k] * 5)
+                k++;
+            listA[++c] = nextU;
+        }
+        return listA[c - 1];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Solution ob = new Solution();
+        System.out.println(ob.getNthUglyNo(n));
+        sc.close();
+    }
+}
